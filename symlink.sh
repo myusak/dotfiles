0488cc066391c206ba3328b9ce62a12f9b691a92
Yusaku Mandai <mandai.yusaku@gmail.com>
2014-11-22 06:45:37 +0900

    improve symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/zsh
 cd $(dirname $0)
 for dotfile in .?*
 do
-	if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.DS_Store' ]
+	if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ]
 	then
-		ln -Fis "$PWD/$dotfile" $HOME
+		ln -Fs "$PWD/$dotfile" $HOME
 	fi
+done
+
+vimtmpdirs=(".vimswap" ".vimundo" ".vimbackup")
+for vimtmpdir in $vimtmpdirs 
+do
+	if [ ! -e "$HOME/$vimtmpdir" ]
+	then
+		echo "$HOME/$vimtmpdir"
+		mkdir -p "$HOME/$vimtmpdir"
+	fi
+
 done
