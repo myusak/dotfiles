7b5f871ef21809a5dcf2f45df841d9862a3ded1f
Yusaku Mandai <mandai.yusaku@gmail.com>
2015-01-22 22:27:13 +0900

    expand tab in symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -2,19 +2,19 @@
 cd $(dirname $0)
 for dotfile in .?*
 do
-	if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ]
-	then
-		ln -Fs "$PWD/$dotfile" $HOME
-	fi
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ]
+    then
+        ln -Fs "$PWD/$dotfile" $HOME
+    fi
 done
 
 vimtmpdirs=(".vimswap" ".vimundo" ".vimbackup")
 for vimtmpdir in $vimtmpdirs 
 do
-	if [ ! -e "$HOME/$vimtmpdir" ]
-	then
-		echo "$HOME/$vimtmpdir"
-		mkdir -p "$HOME/$vimtmpdir"
-	fi
+    if [ ! -e "$HOME/$vimtmpdir" ]
+    then
+        echo "$HOME/$vimtmpdir"
+        mkdir -p "$HOME/$vimtmpdir"
+    fi
 
 done
