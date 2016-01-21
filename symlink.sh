01cd20a2e8dd86d72a125457ab51c73983d85b6d
Yusaku Mandai <mandai.yusaku@gmail.com>
2016-01-21 22:46:07 +0900

    Convert 4 spaces to 1 tab

--- symlink.sh
+++ symlink.sh
@@ -1,33 +1,34 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
+
 cd $(dirname $0)
 for dotfile in .?*
 do
-    if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ] && [ $dotfile != '.config' ]
-    then
-        echo "symlink from $dotfile to $HOME"
-        ln -Fs "$PWD/$dotfile" $HOME
-    fi
+	if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ] && [ $dotfile != '.DS_Store' ] && [ $dotfile != '.config' ]
+	then
+		echo "symlink from $dotfile to $HOME"
+		ln -Fs "$PWD/$dotfile" $HOME
+	fi
 done
 
 vimtmpdirs=(".vimswap" ".vimundo" ".vimbackup")
 for vimtmpdir in $vimtmpdirs
 do
-    if [ ! -e "$HOME/.vim/$vimtmpdir" ]
-    then
-        echo "mkdir $HOME/.vim/$vimtmpdir"
-        mkdir -p "$HOME/.vim/$vimtmpdir"
-    fi
+	if [ ! -e "$HOME/.vim/$vimtmpdir" ]
+	then
+		echo "mkdir $HOME/.vim/$vimtmpdir"
+		mkdir -p "$HOME/.vim/$vimtmpdir"
+	fi
 done
 
 if [ ! -e "$HOME/.config" ]
 then
-    echo "mkdir $HOME/.config"
-    mkdir -p $HOME/.config
+	echo "mkdir $HOME/.config"
+	mkdir -p $HOME/.config
 fi
 
 for conffile in $(cd $(dirname $0) && pwd)/.config/*
 do
-    echo "symlink from $conffile to $HOME/.config"
-    ln -Fs $conffile $HOME/.config
+	echo "symlink from $conffile to $HOME/.config"
+	ln -Fs $conffile $HOME/.config
 done
 
