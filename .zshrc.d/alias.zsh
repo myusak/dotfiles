5258aa10afb8d63586765276f69cc95e5de14427
Yusaku Mandai <mandai.yusaku@gmail.com>
2017-01-31 14:02:32 +0900

    Change short options in alias to long name

--- .zshrc.d/alias.zsh
+++ .zshrc.d/alias.zsh
@@ -1,10 +1,6 @@
-alias less='less -N -r'
+alias less='less --LINE-NUMBERS --raw-control-chars'
 
-alias grep='grep -n --color=auto'
-
-alias ...='../..'
-alias ....='../../..'
-alias .....='../../../..'
+alias grep='grep --line-number --color=auto'
 
 # --color=always does not work in BSD-ls
 alias ls='ls --color=always --group-directories-first'
@@ -12,7 +8,11 @@ alias ll='ls -l --size --human-readable --classify'
 alias la='ls -l --size --human-readable --classify --almost-all'
 alias l='ls -C --classify'
 
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
+alias rm='rm --interactive'
+alias cp='cp --interactive'
+alias mv='mv --interactive'
+
+alias ...='../..'
+alias ....='../../..'
+alias .....='../../../..'
 
